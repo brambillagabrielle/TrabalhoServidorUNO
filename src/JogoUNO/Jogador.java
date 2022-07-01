0083818fa1f85dc37bef96154b33ed5c2eb09039
Gabrielle Brambilla <69489731+brambillagabrielle@users.noreply.github.com>
2022-07-01 10:33:56 -0300

    comeco-jogadas

--- src/JogoUNO/Jogador.java
+++ src/JogoUNO/Jogador.java
@@ -1,27 +1,53 @@
-package JogoUNO;
+package classes;
+
+import java.net.Socket;
 
 public class Jogador {
     
-    int id;
-    String nick;
+    private String id;
+    private String ip;
+    private Socket socket;
+    private String user;
+    private Jogada jogada;
 
-    public Jogador() {
-    }
-
-    public int getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
 
-    public String getNick() {
-        return nick;
+    public String getIp() {
+        return ip;
     }
 
-    public void setNick(String nick) {
-        this.nick = nick;
+    public void setIp(String ip) {
+        this.ip = ip;
     }
+    
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public Jogada getJogada() {
+        return jogada;
+    }
+
+    public void setJogada(Jogada jogada) {
+        this.jogada = jogada;
+    }    
     
 }
